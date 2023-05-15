9cd1b6a00c5f5a2c6ca7b070bf804c8ae2e4fbf9
Aida Mobli <aidaw.mobli@gmail.com>
2023-05-15 20:58:22 +0330

    shortest job first algorithm is dine

---
+++ hw8/SJF.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+struct process{
+    int pid;
+    int init_time;
+    int burst_time;
+    int waiting_time;
+    int response_time;
+}p[10],temp;
+
+int main(){
+    int num_process;
+
+    printf("enter number of process :\t");
+    scanf("%d",&num_process);
+    
+    struct process p[num_process];
+
+    for(int i=0;i<num_process;i++){
+        printf("enter burst time process %i: ",i+1);
+        scanf("%d", &p[i].burst_time);
+        p[i].pid=i+1;
+    }
+
+
+    for (int i = 0; i < num_process; i++)
+    {
+        for (int j = i + 1; j < num_process; j++)
+        {
+            if (p[i].burst_time > p[j].burst_time)
+            {
+                temp = p[i];
+                p[i] = p[j];
+                p[j] = temp;
+            }
+        }
+    }
+    
+    for(int i=0;i<num_process;i++){
+        if(i==0){
+            p[i].waiting_time = 0;
+            p[i].response_time = p[i].burst_time;
+        }
+        else{
+            p[i].waiting_time = p[i-1].response_time;
+            p[i].response_time = p[i].burst_time+p[i].waiting_time;
+        }
+    }
+
+    int total_waiting = 0 , total_response=0;
+    for(int i=0;i<num_process;i++){
+        printf("%i  : burst_time = %i , waiting_time = %i , response_time = %i\n",p[i].pid,p[i].burst_time,p[i].waiting_time,p[i].response_time);
+        total_response += p[i].response_time;
+        total_waiting += p[i].waiting_time;
+    }
+    printf("----------------------\n");
+
+    printf("waiting_average = %f\tresponse_average = %f\n",((float)total_waiting/num_process),((float)total_response/num_process));
+
+    return 0;
+}
